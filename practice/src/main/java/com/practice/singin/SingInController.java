61303bf0c6f9f6b3bf2241e03ecd9f2c586af82a
user <user@LAPTOP-EJD9IR86>
2019-10-15 20:22:53 +0900

    Signin_1

---
+++ practice/src/main/java/com/practice/singin/SingInController.java
@@ -0,0 +1,15 @@
+package com.practice.singin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("signin")
+@Controller
+public class SingInController {
+		
+	@RequestMapping("/signinPage")
+	public String signinPage()
+	{
+		return "singin";
+	}
+}
