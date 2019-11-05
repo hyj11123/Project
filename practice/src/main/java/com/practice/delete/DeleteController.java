670754f16840c093d226722d85d01a54d128d9c0
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2019-11-05 21:04:28 +0900

    re

--- practice/src/main/java/com/practice/delete/DeleteController.java
+++ practice/src/main/java/com/practice/delete/DeleteController.java
@@ -3,12 +3,15 @@ package com.practice.delete;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
- * @RequestMapping("delete")
- * 
- * @Controller public class DeleteController {
- * 
- * @Resource(name="deleteService") private DeleteService cService;
- * 
- * @RequestMapping("/delet") }
- */
+
+//@RequestMapping("delete")
+//@Controller public class DeleteController {
+//	
+//	@Resource(name="deleteService") 
+//	private DeleteService aService;
+//	
+//	@RequestMapping("/delet") 
+//
+//
+//
+//}
