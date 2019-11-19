53fc36c6d96eddc3870c576972820e631c04f433
sts2003 <44109148+sts2003@users.noreply.github.com>
2019-11-19 23:02:50 +0900

    Brand C.R Clear!

--- practice/src/main/java/com/practice/contentmain/chickens/BrandController.java
+++ practice/src/main/java/com/practice/contentmain/chickens/BrandController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("brand")
 @Controller
 public class BrandController {
+	
+	String nullImg = "http://blogfiles.naver.net/MjAxODExMjhfMjc2/MDAxNTQzNDA3NDIzNjk4.4mfvTo5u2uRwNOVDa6MkR3U2VPVScnx032ONVSSqKHUg.-NCmObwXHxF0cEeGdvxUMbSI0aj-Nnm8RwIGRPWlRiQg.JPEG.scenar25/99E7A94F5BFE61D512BAE6.jpeg";
 
 	@Resource(name ="brand_Service")
 	private BrandService bService;
@@ -42,14 +44,18 @@ public class BrandController {
 	
 	@RequestMapping("addBrand")
 	public String addBrand(@RequestParam("name")String name,
-						   @RequestParam("type")String type) 
+						   @RequestParam("type")String type,
+						   @RequestParam(value="path", defaultValue="http://blogfiles.naver.net/MjAxODExMjhfMjc2/MDAxNTQzNDA3NDIzNjk4.4mfvTo5u2uRwNOVDa6MkR3U2VPVScnx032ONVSSqKHUg.-NCmObwXHxF0cEeGdvxUMbSI0aj-Nnm8RwIGRPWlRiQg.JPEG.scenar25/99E7A94F5BFE61D512BAE6.jpeg")String path) 
 	{
 			
 			BrandVO vo = new BrandVO();
 			vo.setName(name);
 			vo.setType(type);
+			vo.setPath(path);
 		
-			return "brand";
+			bService.insertBrand(vo);
+			
+			return "redirect:brandPage";
 
 	}
 	
--- practice/src/main/webapp/WEB-INF/views/brand/insertBrand.jsp
+++ practice/src/main/webapp/WEB-INF/views/brand/insertBrand.jsp
@@ -2,13 +2,13 @@
     pageEncoding="UTF-8"%>
 
     <form action="/brand/addBrand">
-    NAME: <input type="text" name="name"> <br /> 
+    NAME : <input type="text" name="name"> <br /> 
    	TYPE : 
    		<select name = "type">
    			<option value='PIZZA'>PIZZA</option>
    			<option value='CHICKEN'>CHICKEN</option> 	
    		</select>	
-   	
+   PATH : <input type="text" name="path"> <br /> 
    
    <input type="hidden" name="brand_uid" value="4"> <br /> 
    
